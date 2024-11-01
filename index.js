9de3ad5226f74cf70a5e798f99eddb9554004b80
Abdulquddus ibrahim <101833166+Xclusive09@users.noreply.github.com>
2024-11-01 11:39:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 20;
+const pi = 3.14;
+
+let FNAME = "John";
+let LNAME = "Doe";
+let FullName = FNAME + " " + LNAME;
+let total = (num1 + num2) / pi;
+console.log(FullName);
